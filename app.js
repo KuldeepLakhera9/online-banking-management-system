594fe3af3c7cdcc37c90abae0a7540a36b467af8
KuldeepLakhera9 <kuldeeplakhera018@gmail.com>
2026-02-19 17:24:36 +0530

    Phase 8: Ready for deployment

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.get("/dashboard", isLoggedIn, async (req, res) => {
 
 // ================= SERVER =================
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
